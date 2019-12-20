9d8263ce5d747e3661c12fafab4f4b03aa1c9de9
Daniel Drown <dan-android@drown.org>
2019-12-19 18:22:27 -0600

    set local clock from GPS PPS once, then compare

--- esp8266gps.ino
+++ esp8266gps.ino
@@ -2,6 +2,7 @@
 #include <SoftwareSerial.h>
 #include "DateTime.h"
 #include "GPS.h"
+#include "NTPClock.h"
 
 #define BAUD_SERIAL 115200
 #define BAUD_LOGGER 115200
@@ -10,19 +11,19 @@
 
 SoftwareSerial logger(3, 1);
 GPSDateTime gps(&Serial);
+NTPClock localClock;
 
 long lastPPS = 0;
-long startOffset = 0;
 
 //ISR for PPS interrupt
 void ICACHE_RAM_ATTR handleInterrupt() {
   lastPPS = micros();
-  if (startOffset == 0) {
-    startOffset = lastPPS;
-  }
 }
 
+uint32_t compileTime;
 void setup() {
+  DateTime compile = DateTime(__DATE__, __TIME__);
+  
   Serial.begin(BAUD_SERIAL);
   Serial.setRxBufferSize(RXBUFFERSIZE);
   // Move hardware serial to RX:GPIO13 TX:GPIO15
@@ -35,16 +36,41 @@ void setup() {
   pinMode(PPSPIN, INPUT);
   digitalWrite(PPSPIN, LOW);
   attachInterrupt(digitalPinToInterrupt(PPSPIN), handleInterrupt, RISING);  
+  
+  compileTime = compile.ntptime();
+  localClock.setTime(micros(), compileTime);
+  localClock.setPpb(-668); // TODO: linear estimation
+  // allow for compile timezone to be 12 hours ahead
+  compileTime -= 12*60*60;
 }
 
+uint8_t settime = 0;
 void loop() {
   if(Serial.available()) {
     if(gps.decode()) {
-      long now = micros();
-      logger.print(now-lastPPS);
-      logger.print(" ");
-      logger.println(lastPPS-startOffset);
-      gps.GPSnow().print(&logger);
+      if(gps.GPSnow().ntptime() < compileTime) {
+        gps.GPSnow().print(&logger);
+        logger.print(" < ");
+        logger.print(compileTime);
+        logger.println("");
+      } else {
+        if(lastPPS > 0) {
+          logger.println(lastPPS);
+          if(settime) {
+            uint32_t sec, fractional;
+            localClock.getTime(lastPPS, &sec, &fractional);
+            logger.print(sec);
+            logger.print(".");
+            logger.println(fractional);
+          } else {
+            localClock.setTime(lastPPS, gps.GPSnow().ntptime());
+            settime = 1;
+          }
+          lastPPS = 0;
+        }
+        logger.println(gps.GPSnow().ntptime());
+        logger.println("");
+      }
     }
   }
 }
