0c1c281aba6cd75b05613a370aff04e7b0ab05c8
Daniel Drown <dan-android@drown.org>
2019-12-19 21:40:06 -0600

    remove fractional sections from DateTime tests

--- test/test-DateTime.cpp
+++ test/test-DateTime.cpp
@@ -6,37 +6,35 @@ using namespace fakeit;
 #include "DateTime.h"
 
 void test_ntpdate() {
-  DateTime dec2019 = DateTime(3785457371, 1);
+  DateTime dec2019 = DateTime(3785457371);
   TEST_ASSERT_EQUAL(12, dec2019.month());
   TEST_ASSERT_EQUAL(2019, dec2019.year());
-  TEST_ASSERT_EQUAL(1, dec2019.microsfraction());
 
-  DateTime feb2036 = DateTime(4294944000, 2);
+  DateTime feb2036 = DateTime(4294944000);
   TEST_ASSERT_EQUAL(2, feb2036.month());
   TEST_ASSERT_EQUAL(2036, feb2036.year());
-  TEST_ASSERT_EQUAL(2, feb2036.microsfraction());
 }
 
 void test_stringdate() {
-  DateTime dec2019 = DateTime("Dec 26 2019", "12:34:56", 0);
+  DateTime dec2019 = DateTime("Dec 26 2019", "12:34:56");
   TEST_ASSERT_EQUAL(12, dec2019.month());
   TEST_ASSERT_EQUAL(26, dec2019.day());
   TEST_ASSERT_EQUAL(2019, dec2019.year());
 
-  DateTime feb2036 = DateTime("Feb  2 2036", "00:00:00", 0);
+  DateTime feb2036 = DateTime("Feb  2 2036", "00:00:00");
   TEST_ASSERT_EQUAL(2, feb2036.month());
   TEST_ASSERT_EQUAL(2, feb2036.day());
   TEST_ASSERT_EQUAL(2036, feb2036.year());
 }
 
 void test_numberdate() {
-  DateTime dec2019 = DateTime(2019, 12, 26, 12, 34, 56, 0);
+  DateTime dec2019 = DateTime(2019, 12, 26, 12, 34, 56);
   TEST_ASSERT_EQUAL(12, dec2019.month());
   TEST_ASSERT_EQUAL(26, dec2019.day());
   TEST_ASSERT_EQUAL(2019, dec2019.year());
   TEST_ASSERT_EQUAL(1577363696, dec2019.unixtime());
 
-  DateTime feb2036 = DateTime(2036, 2, 2, 0, 0, 0, 0);
+  DateTime feb2036 = DateTime(2036, 2, 2, 0, 0, 0);
   TEST_ASSERT_EQUAL(2, feb2036.month());
   TEST_ASSERT_EQUAL(2, feb2036.day());
   TEST_ASSERT_EQUAL(2036, feb2036.year());
