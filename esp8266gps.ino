37cd0ec7132293bb670319dd67a73bbb59f38579
Daniel Drown <dan-android@drown.org>
2019-12-20 16:39:25 -0600

    log over wifi instead of serial

--- esp8266gps.ino
+++ esp8266gps.ino
@@ -1,9 +1,11 @@
 #include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
 #include <SoftwareSerial.h>
 #include "DateTime.h"
 #include "GPS.h"
 #include "NTPClock.h"
 #include "ClockPID.h"
+#include "settings.h"
 
 #define BAUD_SERIAL 115200
 #define BAUD_LOGGER 115200
@@ -13,6 +15,8 @@
 SoftwareSerial logger(3, 1);
 GPSDateTime gps(&Serial);
 NTPClock localClock;
+WiFiUDP udp;
+IPAddress logDestination;
 
 long lastPPS = 0;
 
@@ -24,7 +28,7 @@ void ICACHE_RAM_ATTR handleInterrupt() {
 uint32_t compileTime;
 void setup() {
   DateTime compile = DateTime(__DATE__, __TIME__);
-
+  
   Serial.begin(BAUD_SERIAL);
   Serial.setRxBufferSize(RXBUFFERSIZE);
   // Move hardware serial to RX:GPIO13 TX:GPIO15
@@ -36,12 +40,32 @@ void setup() {
 
   pinMode(PPSPIN, INPUT);
   digitalWrite(PPSPIN, LOW);
-  attachInterrupt(digitalPinToInterrupt(PPSPIN), handleInterrupt, RISING);
-
+  attachInterrupt(digitalPinToInterrupt(PPSPIN), handleInterrupt, RISING);  
+  
   compileTime = compile.ntptime();
   localClock.setTime(micros(), compileTime);
   // allow for compile timezone to be 12 hours ahead
   compileTime -= 12*60*60;
+
+  logger.print("Connecting to SSID: ");
+  logger.println(ssid);
+  WiFi.begin(ssid, ssidPass);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    logger.print(".");
+  }
+  logger.println();
+
+  logger.print("WiFi connected, IP address: ");
+  logger.println(WiFi.localIP());
+
+  logDestination.fromString(logDestinationIP);
+  udp.begin(123);
+  
+  while(Serial.available()) { // throw away all the text received while starting up
+    Serial.read();
+  }
 }
 
 uint8_t settime = 0;
@@ -60,15 +84,17 @@ void loop() {
             ClockPID.add_sample(lastPPS, gps.GPSnow().ntptime(), offset);
             localClock.setPpb(ClockPID.out() * 1000000000.0);
             double offsetHuman = offset / (double)4294967296.0;
-            logger.print(lastPPS);
-            logger.print(" ");
-            logger.print(offsetHuman, 9);
-            logger.print(" ");
-            logger.print(ClockPID.d(), 9);
-            logger.print(" ");
-            logger.print(ClockPID.d_chi(), 9);
-            logger.print(" ");
-            logger.println(localClock.getPpb());
+            udp.beginPacket(logDestination, 51413);
+            udp.print(lastPPS);
+            udp.print(" ");
+            udp.print(offsetHuman, 9);
+            udp.print(" ");
+            udp.print(ClockPID.d(), 9);
+            udp.print(" ");
+            udp.print(ClockPID.d_chi(), 9);
+            udp.print(" ");
+            udp.println(localClock.getPpb());
+            udp.endPacket();
           } else {
             localClock.setTime(lastPPS, gps.GPSnow().ntptime());
             ClockPID.add_sample(lastPPS, gps.GPSnow().ntptime(), 0);
