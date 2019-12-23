1fa40d4c6695e7aa4d4089f3fe74936218482e97
Dan Drown <dan-android@drown.org>
2019-12-22 20:36:28 -0600

    take the median sample

--- esp8266gps.ino
+++ esp8266gps.ino
@@ -33,7 +33,7 @@ void ICACHE_RAM_ATTR handleInterrupt() {
 uint32_t compileTime;
 void setup() {
   DateTime compile = DateTime(__DATE__, __TIME__);
-  
+
   Serial.begin(BAUD_SERIAL);
   Serial.setRxBufferSize(RXBUFFERSIZE);
   // Move hardware serial to RX:GPIO13 TX:GPIO15
@@ -46,8 +46,8 @@ void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(PPSPIN, INPUT);
   digitalWrite(PPSPIN, LOW);
-  attachInterrupt(digitalPinToInterrupt(PPSPIN), handleInterrupt, RISING);  
-  
+  attachInterrupt(digitalPinToInterrupt(PPSPIN), handleInterrupt, RISING);
+
   compileTime = compile.ntptime();
   localClock.setTime(esp_get_cycle_count(), compileTime);
   // allow for compile timezone to be 12 hours ahead
@@ -69,14 +69,101 @@ void setup() {
 
   logDestination.fromString(logDestinationIP);
   udp.begin(123);
-  
+
   while(Serial.available()) { // throw away all the text received while starting up
     Serial.read();
   }
 }
 
+uint8_t median(int64_t one, int64_t two, int64_t three) {
+  if(one > two) {
+    if(one > three) {
+      if(two > three) {
+        // 1 2 3
+        return 2-1;
+      } else {
+        // 1 3 2
+        return 3-1;
+      }
+    } else {
+      // 3 1 2
+      return 1-1;
+    }
+  } else {
+    if(two > three) {
+      if(one > three) {
+        // 2 1 3
+        return 1-1;
+      } else {
+        // 2 3 1
+        return 3-1;
+      }
+    } else {
+      // 3 2 1
+      return 2-1;
+    }
+  }
+}
+
+#define WAIT_COUNT 3
 uint8_t settime = 0;
-uint8_t wait = 0;
+uint8_t wait = WAIT_COUNT-1;
+struct {
+  int64_t offset;
+  uint32_t pps;
+  uint32_t gpstime;
+} samples[WAIT_COUNT];
+void updateTime(uint32_t gpstime) {
+  if(lastPPS == 0) {
+    return;
+  }
+
+  if(settime) {
+    int64_t offset = localClock.getOffset(lastPPS, gpstime, 0);
+    samples[wait].offset = offset;
+    samples[wait].pps = lastPPS;
+    samples[wait].gpstime = gpstime;
+    if(ClockPID.full() && wait) {
+      wait--;
+    } else {
+      uint8_t median_index = wait;
+      if(wait == 0) {
+        median_index = median(samples[0].offset, samples[1].offset, samples[2].offset);
+      }
+      ClockPID.add_sample(samples[median_index].pps, samples[median_index].gpstime, samples[median_index].offset);
+      localClock.setRefTime(samples[median_index].gpstime);
+      localClock.setPpb(ClockPID.out() * 1000000000.0);
+      wait = WAIT_COUNT-1; // (2+1)*16=48s, 80MHz wraps at 53s
+
+      double offsetHuman = samples[median_index].offset / (double)4294967296.0;
+      udp.beginPacket(logDestination, 51413);
+      udp.print(samples[median_index].pps);
+      udp.print(" ");
+      udp.print(offsetHuman, 9);
+      udp.print(" ");
+      udp.print(ClockPID.d(), 9);
+      udp.print(" ");
+      udp.print(ClockPID.d_chi(), 9);
+      udp.print(" ");
+      udp.print(localClock.getPpb());
+      udp.print(" ");
+      udp.println(samples[median_index].gpstime);
+      udp.endPacket();
+    }
+  } else {
+    localClock.setTime(lastPPS, gpstime);
+    ClockPID.add_sample(lastPPS, gpstime, 0);
+    settime = 1;
+    udp.beginPacket(logDestination, 51413);
+    udp.print("S ");
+    udp.print(lastPPS);
+    udp.print(" ");
+    udp.println(gpstime);
+    udp.endPacket();
+  }
+  lastPPS = 0;
+}
+
 uint32_t lastUpdate = 0;
 void loop() {
   server.poll();
@@ -97,46 +184,7 @@ void loop() {
         udp.println(gpstime);
         udp.endPacket();
       } else {
-        if(lastPPS > 0) {
-          if(settime) {
-            int64_t offset = localClock.getOffset(lastPPS, gpstime, 0);
-            if(ClockPID.full() && wait) {
-              wait--;
-            } else {
-              ClockPID.add_sample(lastPPS, gpstime, offset);
-              localClock.setRefTime(gpstime);
-              localClock.setPpb(ClockPID.out() * 1000000000.0);
-              wait = 2; // (2+1)*16=48s, 80MHz wraps at 53s
-            }
-            double offsetHuman = offset / (double)4294967296.0;
-            udp.beginPacket(logDestination, 51413);
-            udp.print(lastPPS);
-            udp.print(" ");
-            udp.print(offsetHuman, 9);
-            udp.print(" ");
-            udp.print(ClockPID.d(), 9);
-            udp.print(" ");
-            udp.print(ClockPID.d_chi(), 9);
-            udp.print(" ");
-            udp.print(localClock.getPpb());
-            udp.print(" ");
-            udp.print(gpstime);
-            udp.print(" ");
-            udp.println(wait);
-            udp.endPacket();
-          } else {
-            localClock.setTime(lastPPS, gpstime);
-            ClockPID.add_sample(lastPPS, gpstime, 0);
-            settime = 1;
-            udp.beginPacket(logDestination, 51413);
-            udp.print("S ");
-            udp.print(lastPPS);
-            udp.print(" ");
-            udp.println(gpstime);
-            udp.endPacket();
-          }
-          lastPPS = 0;
-        }
+        updateTime(gpstime);
       }
     }
   }
